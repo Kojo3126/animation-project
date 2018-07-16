632d39f5372f935efff5af7047e950ba5ef54bec
Riya Koshy <riyakoshy@Riyas-MacBook-Air.local>
2018-07-16 15:07:40 -0400

    a summary of what I did

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.querySelector('button').addEventListener('click', function()
+document.querySelector('.colbutton').addEventListener('click', function()
 	{
     	var x = Math.floor(Math.random() * 255);
     	var y = Math.floor(Math.random() * 255);
@@ -7,3 +7,18 @@ document.querySelector('button').addEventListener('click', function()
   
   document.querySelector('html').style.background = Color;
     });
+
+var isInDarkMode = false;
+
+document.querySelector('.nightbutton').addEventListener('click', function() {
+	if (isInDarkMode) {
+		document.querySelector('html').style.background = "white";
+		document.querySelector('html').style.color= "black";
+		isInDarkMode = false
+	} else {
+
+	document.querySelector('html').style.background = "black";
+	document.querySelector('html').style.color = "beige";
+	isInDarkMode = true;
+}
+});
