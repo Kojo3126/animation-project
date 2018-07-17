3e0b302fa01c06c1abff25588cd324a3fd8caa44
Ryan <kimr@bxscience.edu>
2018-07-17 16:23:24 -0400

    l

--- script.js
+++ script.js
@@ -12,13 +12,13 @@ var isInDarkMode = false;
 
 document.querySelector('.nightbutton').addEventListener('click', function() {
 	if (isInDarkMode) {
-		document.querySelector('html').style.background = "black";
-		document.querySelector('html').style.color= "gold";
+		document.querySelector('html').style.background = "white";
+		document.querySelector('html').style.color= "black";
 		isInDarkMode = false
 	} else {
 
-	document.querySelector('html').style.background = "white";
-	document.querySelector('html').style.color = "black";
+	document.querySelector('html').style.background = "black";
+	document.querySelector('html').style.color = "beige";
 	isInDarkMode = true;
 }
 });
