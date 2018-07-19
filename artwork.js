3eae41a0fb02b9e8274189095f370155d5fb8975
Krisanta Ojo <KrisyK@Krisantas-MacBook-Air.local>
2018-07-19 12:00:28 -0400

    a summary of what I did

--- artwork.js
+++ artwork.js
@@ -26,13 +26,15 @@ document.querySelector('.nightbutton').addEventListener('click', function() {
 
 document.querySelector('.contemporary').addEventListener('click', function(){
 	var randomImage =["1.jpg", "2.jpg", "3.jpg" , "4.jpg" ];
-	var randomInfo= ["Olafur Eliasson, The weather project, 2003" ,  "Rachel Harrison, Huffy Howler, 2004" ,
-	" Kehinde Wiley, Napoleon Leading the Army over the Alps, 2005" , " Kara Walker, A Subtlety, 2014"];
+	var randomInfo= ["Olafur Eliasson, The weather project, 2003 Eliasson created this piece with the intent of bringing a part of his home city of London to New York. This artwork was made with different weather factors such as wind, rain, and sun in mind. This piece is an attempt to convey the way weather shapes cities and how people experience weather in cities.  In this project he decided to direct attention away from information cards in front of the exhibition, so that the viewers can experience the work unscathed. " ,  "Rachel Harrison, Huffy Howler, 2004 Rachel Harrison intended to make a found-object sculpture representing the concept of the equestrian statue from ancient imperial states. Harrison includes black purses full of rocks, a bike with a flat front tire, and an aluminum pole holding up synthetic sheepskin with a printed photograph of Mel Gibson hanging over the sheepskin. The sculpture has many layers of meaning which Harrison leaves up to the imagination of viewers. Huffy Howler is currently located within the Walker Art Center in Minneapolis, Minnesota" ,
+	" Kehinde Wiley, Napoleon Leading the Army over the Alps, 2005 Napoleon Leading the Army over the Alps, located in the Brooklyn Museum is Kehinde Wiley’s recreation of a painting by Jacques-Louis David in which he depicted Napoleon’s victory over the Austrians.  In Wiley’s painting he replaces Napoleon with an African American man in order to encourage viewers to think about the biases art has historically and how race plays a part in  “masterpiece” art. In the words of Wiley himself, this painting is “about the world that we live in. Black men live in the world. My choice is to include them."
+     , " Kara Walker, A Subtlety, 2014 A Subtlety (aka the Marvelous Sugar Baby) is a piece of installation art by American artist Kara Walker. The art’s central piece is a white sculpture depicting a woman with African features in the shape of a sphinx but also includes fifteen other sculptures. The piece was installed in the Domino Sugar Factory in the Williamsburg neighborhood of Brooklyn from May through July 2014. The exhibition sparked conversations about the show’s audience, the gentrification of Brooklyn and the work’s themes of race, sexuality, oppression, labor and the ephemeral. "]; 
 	var randomNumber = Math.floor(Math.random() * 4) + 1;
     var imgName =  randomNumber + ".jpg";
     var quote = randomInfo[randomNumber]
     document.getElementById("art").src= imgName;
     document.getElementById("artDescription").textContent= quote;
+
  });
 
 
